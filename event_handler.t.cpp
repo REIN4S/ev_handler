57a5ed375e1e8ce71132eaaf5311aa31f5dfbcba
REIN4S <37785829+REIN4S@users.noreply.github.com>
2021-09-07 01:32:16 +0900

    Update event_handler.t.cpp

--- event_handler.t.cpp
+++ event_handler.t.cpp
@@ -1,40 +1,62 @@
 #include <stdio.h>
 #include "event_handler.hpp"
 
+typedef function<void(string)>   delegator_t;
+typedef function<void(void*)>   callback_t;
+typedef function<void(void*)>     lambda_t;
+EventHandler<delegator_t, string> *g_delegator_listeners;
+EventHandler<callback_t, void*> *g_callback_listeners;
+EventHandler<lambda_t, void*> *g_lambda_listeners;
+void run();
+
 void delegator__process_A(string data) {
 	fprintf(stderr, "%s() delegator's data: %s\n", __FUNCTION__, data.c_str());
 };
 
-void cb__test(void *data) {
-	fprintf(stderr, "%s() callback's data: %s\n", __FUNCTION__, data);
+void cb__test(void* data) {
+	fprintf(stderr, "%s() callback's data: %d\n", __FUNCTION__, data);
 };
 
 int main(int argc, char **argv)
 {
 	// 1. INIT
-	EventHandler<delegator_t, string> *g_delegator_listeners = new EventHandler<delegator_t, string>();
-	EventHandler<callback_t, void*> *g_callback_listeners = new EventHandler<callback_t, void*>();
-	EventHandler<lambda_t, void*> *g_lambda_listeners = new EventHandler<lambda_t, void*>(); 
+	g_delegator_listeners = new EventHandler<delegator_t, string>();
+	g_callback_listeners = new EventHandler<callback_t, void*>();
+	g_lambda_listeners = new EventHandler<lambda_t, void*>(); 
 
 	// 2. CREATE
-	string message = R"({"message" : 123 })";
-	g_delegator_listeners->register_event("delegator test", delegator__process_A, message);
-	string data = R"({"data" : 123 })";
-	g_delegator_listeners->register_event("delegator test2", delegator__process_A, data);
-	g_callback_listeners->register_event("callback test", cb__test, (void*)"123");
-	g_lambda_listeners->register_event("lambda test", [](void* data){
-		fprintf(stderr, "%s() fn's data: %s\n", __FUNCTION__, data);
+	g_delegator_listeners->register_event("delegator test", delegator__process_A, R"({"message" : 123 })");
+	// g_delegator_listeners->register_event("delegator test2", delegator__process_A, R"({"data" : 123 })");
+	g_callback_listeners->register_event("callback test", cb__test);
+	g_callback_listeners->register_event("callback test2", cb__test);
+	g_callback_listeners->register_event("callback test3", cb__test);
+	string capture = "captured data";
+	g_lambda_listeners->register_event("lambda test", [&capture](void* data){
+		fprintf(stderr, "%s() lambda's data: %s \t and captured data: %s\n", __FUNCTION__, data, capture.c_str());
 	}, (void*)"123");
 
 	// 3. RUN
-	g_delegator_listeners->rewind_event();
-	g_delegator_listeners->rewind_event();
-	// g_delegator_listeners->action_event();
-	// g_delegator_listeners->filter_event("delegator test");
+	run();	
 
 	// 4. DONE
 	delete g_delegator_listeners;
 	delete g_callback_listeners;
 	delete g_lambda_listeners;
 	return 0;
+}
+
+void run() {
+	cout << "delegator - FIRST" << endl;
+	g_delegator_listeners->rewind_event();
+	cout << "delegator - SECOND" << endl;
+	g_delegator_listeners->rewind_event();
+
+	cout << "callback - FIRST" << endl;
+	g_callback_listeners->rewind_event((void*)123);
+
+	cout << "lambda - FIRST" << endl;
+	g_lambda_listeners->rewind_event();
+	g_lambda_listeners->rewind_event((void*)"asdf");
+	// g_delegator_listeners->action_event();
+	// g_delegator_listeners->filter_event("delegator test");
 }
